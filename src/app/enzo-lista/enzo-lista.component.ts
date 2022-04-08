b64074db221b41e56dfa907323836da7158cd343
enzocsantos18 <enzocsantos18@gmail.com>
2022-04-08 19:50:52 -0300

    3 files added

---
+++ src/app/enzo-lista/enzo-lista.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-enzo-lista',
+  templateUrl: './enzo-lista.component.html',
+  styleUrls: ['./enzo-lista.component.css'],
+})
+export class EnzoListaComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
