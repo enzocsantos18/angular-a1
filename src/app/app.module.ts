32b0cac079b3c73579a00ff16fc3bafc45806076
enzocsantos18 <enzocsantos18@gmail.com>
2022-04-08 19:52:17 -0300

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,23 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { RouterModule } from '@angular/router';
 import { EnzoComponent } from './enzo/enzo.component';
+import { EnzoListaComponent } from './enzo-lista/enzo-lista.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([{ path: 'enzo', component: EnzoComponent }]),
+    RouterModule.forRoot([
+      { path: 'enzo', component: EnzoComponent },
+      { path: 'disciplinas', component: EnzoListaComponent },
+    ]),
   ],
-  declarations: [AppComponent, HelloComponent, EnzoComponent],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    EnzoComponent,
+    EnzoListaComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
